d23836cf446f501ee7508900cda7de7f986fde28
LiyaSabit <90717232+LiyaSabit@users.noreply.github.com>
2021-10-17 01:11:05 +0600

    Add files via upload

---
+++ Lab4/d.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+    int val;
+    node *left;
+    node *right;
+    node(int x){
+        val = x;
+        left = right = NULL;
+    }
+};
+
+class BST{
+    node *cur;
+
+    node *insert(node *cur, int val){
+        if(cur == NULL){
+            return new node (val);//после ретерна ничего не выполнится
+        }
+        if(val > cur-> val){
+            cur -> right = insert(cur -> right,val);
+        }
+        if(val < cur -> val){
+            cur -> left = insert(cur -> left, val);
+        }
+    return cur;
+    }
+
+    void inOrder(node *cur){
+        if(cur == NULL){
+            return;
+        }
+        inOrder(cur -> left);
+        cout << cur -> val << endl;
+        inOrder(cur -> right);
+        return;
+    }
+public:
+    BST(){
+        cur = NULL;
+    }
+    void insert(int val){
+        cur = insert(cur, val);
+    }
+    void printAsc(){
+        inOrder(cur);
+    }
+};
+int main(){
+    BST tree;
+    int x;
+    cin >> x;
+    while(x != 0){
+        tree.insert(x);
+        cin >> x;
+    }
+    tree.printAsc();
+}
